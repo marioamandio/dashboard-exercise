448fd02e0cb41765e4c50bc9fd658b196ae537cb
marioamandio <30850605+marioamandio@users.noreply.github.com>
2019-01-04 20:46:46 +0000

    update some small issues

--- server/database/config.js
+++ server/database/config.js
@@ -3,7 +3,7 @@ const databaseConfig = {}
 if(process.env.NODE_ENV === 'production') {
 
     databaseConfig.host = 'us-cdbr-iron-east-01.cleardb.net';
-    databaseConfig.user = 'b63208a7b9abad:892a279d';
+    databaseConfig.user = 'b63208a7b9abad';
     databaseConfig.password = '892a279d';
     databaseConfig.database = 'heroku_e3178021a2fd2ac';
 
